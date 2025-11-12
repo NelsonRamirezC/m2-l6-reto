b07ec5e14a2af380726bd6392bf4fde707e05c41
Nelson Ramirez <xhyskoax@gmail.com>
2025-11-12 15:36:39 -0300

    estructura base inicio reto

---
+++ assets/js/productos.js
@@ -0,0 +1,19 @@
+let producto1 = {
+    "id": 1,
+    "nombre": "Producto 1",
+    "descripcion": "Descripción producto 1.",
+    "precio": 1500,
+    "urlImagen": "https://images.pexels.com/photos/2533266/pexels-photo-2533266.jpeg"
+}
+
+let producto2 = {
+    "id": 2,
+    "nombre": "Producto 2",
+    "descripcion": "Descripción producto 2.",
+    "precio": 5500,
+    "urlImagen": "https://images.pexels.com/photos/1667071/pexels-photo-1667071.jpeg"
+}
+
+
+const listaProductos = [producto1, producto2];
+
