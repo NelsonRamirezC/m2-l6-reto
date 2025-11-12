a5bbe67cc765476dd82ce0111f3a2e9f03260aa7
Nelson Ramirez <xhyskoax@gmail.com>
2025-11-12 16:31:57 -0300

    Se agrega lógica de enlace de las tarjetas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-console.log("Productos", listaProductos);
+$(function() {
+    
+    
+
+    $("#seccion-productos .card a").click(function(event){
+        event.preventDefault();
+        
+        let continuar = confirm("Está seguro que quiere ir a la página de detalles");
+
+        if(continuar){
+            // SE EJECUTA SIEMPRE QUE SEA VERDADERO
+
+            let url = $(this).attr("href");
+            window.location.href = url;
+        }
+
+    });
+    
+
+
+
+});
